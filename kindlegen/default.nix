3d6a06f752d790aff53429285dd026837034d43a
nofishleft <rishaan.gupta@gmail.com>
2025-11-23 16:03:56 +1300

    Kindlegen hash

--- kindlegen/default.nix
+++ kindlegen/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
   
   src = fetchurl {
     url = "https://archive.org/download/kindlegen2.9/kindlegen_linux_2.6_i386_v2_9.tar.gz";
-    hash = "sha256-a";
+    hash = "sha256-u92xoQEFgcnmIsddhv6GZ149yt9F37I9eNn9Ne/xy8U=";
   };
 
   dontConfigure = true;
